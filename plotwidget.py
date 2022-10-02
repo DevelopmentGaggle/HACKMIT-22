2d468e16db1ae9af88ba6404e23a06f7c2c866bb
Oren <oren.b.anderson@gmail.com>
2022-10-02 03:08:35 -0400

    big yikes

--- plotwidget.py
+++ plotwidget.py
@@ -1,21 +1,12 @@
-import sys
-from PyQt6.QtCore import QSize
-import matplotlib
-matplotlib.use('Qt5Agg')
-
 from PyQt6 import QtCore, QtWidgets
 
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
 
-class MplCanvas(FigureCanvasQTAgg):
+class MplCanvas(FigureCanvas):
 
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
         super(MplCanvas, self).__init__(fig)
-
-    def sizeHint(self):
-        return QSize(1, 1)
-
--- test.py
+++ test.py
@@ -1,44 +1,52 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout, QWidget
-from PyQt6.QtGui import QPixmap, QImage
-import requests
-import WikiHandler
-from bing_image_urls import bing_image_urls
+import random
+import matplotlib
+matplotlib.use('Qt5Agg')
 
-class Example(QWidget):
+from PyQt6 import QtCore, QtWidgets
 
-    def __init__(self):
-        super().__init__()
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 
-        page, image_source = WikiHandler.get_first_wiki("Franklin")
-        #image_source = "https://upload.wikimedia.org/wikipedia/commons/6/6d/2016-06-06_09_38_36_View_south_along_U.S._Route_220_%28Main_Street%29_just_south_of_Pine_Street_in_Franklin%2C_Pendleton_County%2C_West_Virginia.jpg"
-        #image_source = "https://upload.wikimedia.org/wikipedia/commons/1/1c/Boethius.consolation.philosophy.jpg"
 
-        image = QImage()
+class MplCanvas(FigureCanvas):
 
-        url = bing_image_urls(page.title, limit=1)[0]
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
 
-        image.loadFromData(requests.get(url).content)
 
-        #print(page.title)
+class MainWindow(QtWidgets.QMainWindow):
 
-        pixmap = QPixmap(image)
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
 
-        pixmap = pixmap.scaledToHeight(64)
+        self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
+        self.setCentralWidget(self.canvas)
 
-        self.im = pixmap
-        self.label = QLabel()
-        self.label.setPixmap(self.im)
+        n_data = 50
+        self.xdata = list(range(n_data))
+        self.ydata = [random.randint(0, 10) for i in range(n_data)]
+        self.update_plot()
 
-        self.grid = QGridLayout()
-        self.grid.addWidget(self.label, 1, 1)
-        self.setLayout(self.grid)
-
-        self.setGeometry(50, 50, 320, 200)
-        self.setWindowTitle("PyQT show image")
         self.show()
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec())
+        # Setup a timer to trigger the redraw by calling update_plot.
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start()
+
+    def update_plot(self):
+        # Drop off the first y element, append a new one.
+        self.ydata = self.ydata[1:] + [random.randint(0, 10)]
+        self.canvas.axes.cla()  # Clear the canvas.
+        self.canvas.axes.plot(self.xdata, self.ydata, 'r')
+        # Trigger the canvas to update and redraw.
+        self.canvas.draw()
+
+
+app = QtWidgets.QApplication(sys.argv)
+w = MainWindow()
+app.exec()
