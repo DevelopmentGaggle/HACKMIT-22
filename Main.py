7d603eec048a08727a9282d6f05d70f9762198fe
StevenPhang22 <sphang@ufl.edu>
2022-10-01 20:18:52 -0400

    fixed main

--- Main.py
+++ Main.py
@@ -2,17 +2,18 @@ from PyQt6 import QtWidgets, uic
 from PyQt6.QtGui import QImage, QPixmap
 import WikiHandler
 import requests
+import StyleSheet
+
 
 class MainWindowUI(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindowUI, self).__init__()
         uic.loadUi("MainWindow.ui", self)
-        self.pushButton.clicked.connect(self.print_button_pressed)
+        self.runButton.clicked.connect(self.print_button_pressed)
 
     def print_button_pressed(self):
-        source_widget = SourceUI("Ford")
+        source_widget = SourceUI("Franklin")
         self.verticalLayout.addWidget(source_widget)
-
 
 
 class SourceUI(QtWidgets.QWidget):
@@ -29,27 +30,28 @@ class SourceUI(QtWidgets.QWidget):
 
             pixmap = QPixmap(image)
 
-            pixmap = pixmap.scaledToHeight(64) # this value will need to coordinate with the ui file
+            # this value will need to coordinate with the ui file
+            pixmap = pixmap.scaledToHeight(64)
 
             self.label.setText("")
             self.label.setPixmap(pixmap)
             print("attempted to set picture")
 
-
         # Set text in the text browser to the summary of the article (for now)
         self.textBrowser.setText(page.summary)
 
-
         # Set up the slot for signal/slot for the deleting push button
-        #self.pushButton
+        self.stopButton.clicked.connect(self.delete_widget_func)
 
-
+    def delete_widget_func(self):
+        self.setParent(None)
 
 
 def main():
     # Set up asynchronous processes here
 
     app = QtWidgets.QApplication([])
+    app.setStyleSheet(StyleSheet.StyleSheet)
     window = MainWindowUI()
     window.show()
     app.exec()
@@ -57,4 +59,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
