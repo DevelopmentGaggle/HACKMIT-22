7ee6fda2def38c0d10ecd4cc3c2e0310b7a7c682
Oren <oren.b.anderson@gmail.com>
2022-10-01 20:39:09 -0400

    Steven makes me sad sometimes

---
+++ StyleSheet.py
@@ -0,0 +1,39 @@
+StyleSheet = '''
+QPushButton {
+    border-radius: 5px;
+    color: #ffffff;
+    font-weight: bold;
+}
+QFrame {
+    background-color: #EEEEEE;
+    border: 1px;
+    border-radius: 10px;
+}
+QAbstractScrollArea {
+    border: 10px solid white;
+    border-radius: 10px;
+}
+QWidget#scrollAreaWidgetContents {
+    background-color: #ffffff;
+    border: 1px solid white;
+}
+QPushButton#stopButton {
+    background-color: #f44336;
+}
+#stopButton:hover {
+    background-color: #e57373;
+}
+#stopButton:pressed { 
+    background-color: #ffcdd2;
+}
+#runButton {
+    background-color: #4caf50;
+}
+#runButton:hover {
+    background-color: #81c784;            
+}
+#runButton:pressed {
+    background-color: #c8e6c9;
+}
+
+'''
