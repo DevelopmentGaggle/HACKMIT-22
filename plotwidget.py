46db41572736d6f7f48629a25fc1f9dfdd5ec0f6
StevenPhang22 <sphang@ufl.edu>
2022-10-02 07:06:24 -0400

    added word cloud

--- plotwidget.py
+++ plotwidget.py
@@ -10,3 +10,10 @@ class MplCanvas(FigureCanvas):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
         super(MplCanvas, self).__init__(fig)
+
+class MplCanvasSpaghetti(FigureCanvas):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvasSpaghetti, self).__init__(fig)
